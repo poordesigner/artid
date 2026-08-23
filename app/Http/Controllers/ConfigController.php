3fdf7438b5555327e0965fd9b1d23b7c89d84f63
poordesigner <113371487+poordesigner@users.noreply.github.com>
2026-08-23 15:58:16 -0500

    feat: tabs Seguridad/Planes en /configuracion

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plan;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -9,8 +10,17 @@ class ConfigController extends Controller
 {
     public function index(Request $request): View
     {
+        $plans = [];
+
+        if ($request->user()?->isAdmin()) {
+            $plans = Plan::with(['periods', 'features', 'legalTerms'])
+                ->orderBy('sort_order')
+                ->get();
+        }
+
         return view('configuracion.index', [
             'user' => $request->user(),
+            'plans' => $plans,
         ]);
     }
 }
